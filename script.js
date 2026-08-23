ff62dae454126839eeb0c50c9e59632bef9c463e
Danielle Higgins <70040334+Danielle-Higgins@users.noreply.github.com>
2026-08-22 21:40:20 -0400

    Create function to get computer choice
    This function will randomly return the string
    values of rock, paper, and scissors.

---
+++ script.js
@@ -0,0 +1,22 @@
+/**
+ * Create function to get computer choice
+ *      Use Math.random to get a random integer between 1-3 and store inside variable compChoice
+ *      If compChoice is 1, return string rock
+ *      Else If compChoice is 2, return string paper
+ *      Else return string scissors
+ */
+
+// Function randomly returns “rock”, “paper” or “scissors”
+function getComputerChoice() {
+  const compChoice = Math.floor(Math.random() * 3) + 1;
+
+  if (compChoice === 1) {
+    return "rock";
+  } else if (compChoice === 2) {
+    return "paper";
+  } else {
+    return "scissors";
+  }
+}
+
+console.log(getComputerChoice());
