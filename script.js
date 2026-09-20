bc09b79e66b65dc741a59e8af215e7ed40d05d73
Danielle Higgins <70040334+Danielle-Higgins@users.noreply.github.com>
2026-09-20 19:49:33 -0400

    add user selection functionality

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-/**
- * Create function to get computer choice
- *      Use Math.random to get a random integer between 1-3 and store inside variable compChoice
- *      If compChoice is 1, return string rock
- *      Else If compChoice is 2, return string paper
- *      Else return string scissors
- */
-
-// Function randomly returns “rock”, “paper” or “scissors”
 function getComputerChoice() {
   const compChoice = Math.floor(Math.random() * 3) + 1;
 
@@ -19,24 +10,10 @@ function getComputerChoice() {
   }
 }
 
-/**
- * Create function to get the human choice
- *      Create empty string
- *      While true, prompt the user for rock, paper, or scissors and convert to lowercase
- *          if the choice is rock, paper, or scissors, break out of loop
- *      return the choice
- */
-
-// Function that takes the user choice and returns it
-function getHumanChoice() {
-  let choice = "";
-
-  while (true) {
-    choice = prompt("Enter rock, paper, or scissors:", "").toLowerCase();
-
-    if (choice === "rock" || choice === "paper" || choice === "scissors") break;
-  }
-  return choice;
+function setEmoji(choice) {
+  if (choice === "rock") return "✊";
+  else if (choice === "paper") return "✋";
+  else return "✌️";
 }
 
 function playGame() {
@@ -77,4 +54,20 @@ function playGame() {
   }
 }
 
-// playGame();
+const weapons = document.querySelectorAll(".btn-container div");
+const userEmoji = document.querySelector("#user-choice");
+const compEmoji = document.querySelector("#comp-choice");
+const userSelectText = document.querySelector("#user-selection");
+const compSelectText = document.querySelector("#comp-selection");
+
+weapons.forEach((weapon) => {
+  weapon.addEventListener("click", () => {
+    const userChoice = weapon.firstElementChild.id;
+    const compChoice = getComputerChoice();
+
+    userEmoji.textContent = setEmoji(userChoice);
+    compEmoji.textContent = setEmoji(compChoice);
+    userSelectText.textContent = userChoice;
+    compSelectText.textContent = compChoice;
+  });
+});
